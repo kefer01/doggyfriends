35cdbc186215e2177da74e99aea93b822ec609f6
DOU-27 <104288205+DOU-27@users.noreply.github.com>
2023-03-01 23:31:08 -0600

    Cambio Varios Imagenes

--- views/layout.php
+++ views/layout.php
@@ -6,10 +6,11 @@ $auth = $_SESSION['login'] ?? false;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UpTask | <?php echo $titulo ?? ''; ?></title>
+    <title>DoggyFiends |<?php echo $titulo ?? ''; ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="build/css/app.css">
 </head>
 <body>
@@ -18,7 +19,7 @@ $auth = $_SESSION['login'] ?? false;
             <div class="barra">
                 <div class="izquierda">
                     <a href="/">
-                        <img src="/build/img/logo.png" alt="Logotipo Doggy Friends">
+                        <img src="/build/img/LOGO DOGGY.png" alt="Logotipo Doggy Friends">
                     </a>
                 </div>
                 <div class="mobile-menu">
@@ -58,6 +59,9 @@ $auth = $_SESSION['login'] ?? false;
         </div>
         <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
     </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <?php echo $script ?? ''; ?>
 
 </body>
